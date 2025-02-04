d5cc0a3dcab9175c23d3b6e2ffbbd4c37c5f9ee0
ilhamhafidz404 <xxspanzie@gmail.com>
2025-02-04 16:54:50 +0700

    Section Course Article & Video

--- resources/js/Pages/LandingPage.tsx
+++ resources/js/Pages/LandingPage.tsx
@@ -3,6 +3,9 @@ import FooterComponent from '@/Components/Footer';
 import NavbarComponent from '@/Components/Navbar';
 import { Head } from '@inertiajs/react';
 
+import 'glider-js/glider.min.css';
+import Glider from 'react-glider';
+
 export default function LandingPage() {
     return (
         <>
@@ -10,7 +13,7 @@ export default function LandingPage() {
 
             <NavbarComponent />
 
-            <header className="relative min-h-screen pt-16">
+            <header className="relative min-h-screen pt-16 xl:min-h-[800px]">
                 <img
                     src="/images/shapes/blueBlur.png"
                     alt=""
@@ -81,29 +84,76 @@ export default function LandingPage() {
                 </div>
             </header>
 
-            <section className="container mx-auto grid grid-cols-4 gap-10">
-                <div className="col-span-4">
-                    <h2 className="text-2xl font-semibold">
+            <section className="container relative mx-auto mb-20 gap-10 px-3 md:px-10 xl:px-0">
+                <div className="mb-5">
+                    <h2 className="text-xl font-semibold md:text-2xl">
+                        <span className="text-gray-400">//</span> Tonton Video
+                        Tutorial Seputar Programming
+                    </h2>
+                </div>
+                <Glider
+                    draggable
+                    slidesToShow={4.5}
+                    slidesToScroll={1}
+                    // hasArrows
+                    dragVelocity={1.5}
+                    responsive={[
+                        { breakpoint: 1536, settings: { slidesToShow: 4.5 } }, // Desktop
+                        { breakpoint: 1280, settings: { slidesToShow: 4.2 } }, // Desktop
+                        { breakpoint: 1024, settings: { slidesToShow: 3.3 } }, // Desktop
+                        { breakpoint: 768, settings: { slidesToShow: 2.3 } }, // Tablet
+                        { breakpoint: 375, settings: { slidesToShow: 1.3 } }, // Mobile
+                        { breakpoint: 100, settings: { slidesToShow: 1.1 } }, // Mobile
+                    ]}
+                >
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                    <CardComponent type="video" />
+                </Glider>
+                <div className="absolute -right-3 top-0 h-full w-24 bg-gradient-to-r from-gray-100/0 to-gray-100 md:w-36"></div>
+            </section>
+
+            <section className="container relative mx-auto mb-20 gap-10 px-3 md:px-10 xl:px-0">
+                <div className="mb-5">
+                    <h2 className="text-xl font-semibold md:text-2xl">
                         <span className="text-gray-400">//</span> Temukan
                         Artikel Menarik Seputar Programming
                     </h2>
                 </div>
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
+                <Glider
+                    draggable
+                    slidesToShow={4.5}
+                    slidesToScroll={1}
+                    // hasArrows
+                    dragVelocity={1.5}
+                    responsive={[
+                        { breakpoint: 1536, settings: { slidesToShow: 4.5 } }, // Desktop
+                        { breakpoint: 1280, settings: { slidesToShow: 4.2 } }, // Desktop
+                        { breakpoint: 1024, settings: { slidesToShow: 3.3 } }, // Desktop
+                        { breakpoint: 768, settings: { slidesToShow: 2.3 } }, // Tablet
+                        { breakpoint: 375, settings: { slidesToShow: 1.3 } }, // Mobile
+                        { breakpoint: 100, settings: { slidesToShow: 1.1 } }, // Mobile
+                    ]}
+                >
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                    <CardComponent type="article" />
+                </Glider>
+                <div className="absolute -right-3 top-0 h-full w-24 bg-gradient-to-r from-gray-100/0 to-gray-100 md:w-36"></div>
             </section>
 
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
             <FooterComponent />
         </>
     );
