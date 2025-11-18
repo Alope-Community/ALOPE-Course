a2c1241e1ac4abb8046d67ea0d186be1d364af79
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-11-18 22:40:27 +0700

    Redesign footer

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -1,202 +1,95 @@
-import {
-    IconBrandDiscord,
-    IconBrandGithub,
-    IconBrandInstagram,
-} from 'justd-icons';
+import { IconMail, IconMap, IconPilone } from 'justd-icons';
 
 export default function FooterComponent() {
     return (
-        // <footer className="container mx-auto mt-20 flex flex-wrap gap-10 px-3 py-14 text-center md:px-10 lg:flex-nowrap lg:gap-5 lg:text-left xl:px-5 2xl:px-2">
-        //     <div className="order-1 w-full lg:w-1/4">
-        //         <p className="text-3xl font-bold">ALOPE</p>
-        //         <p>Tempat belajar koding yang menyenangkan!</p>
-        //         <div className="hidden lg:block">
-        //             {/* <img
-        //                 src="/images/contactList.png"
-        //                 alt="contact-list"
-        //                 className="mt-10"
-        //             /> */}
-        //             <div className="mt-5 flex gap-4 text-gray-700">
-        //                 <a href="https://discord.gg/WV7wAdcp" target="_blank">
-        //                     <IconBrandDiscord className="size-7" />
-        //                 </a>
-        //                 <a
-        //                     href="https://www.instagram.com/alope.world/"
-        //                     target="_blank"
-        //                 >
-        //                     <IconBrandInstagram className="size-7" />
-        //                 </a>
-        //                 <a
-        //                     href="https://github.com/orgs/Alope-Community/dashboard"
-        //                     target="_blank"
-        //                 >
-        //                     <IconBrandGithub className="size-7" />
-        //                 </a>
-        //             </div>
-        //             <p className="mt-5">&copy; 2025</p>
-        //         </div>
-        //     </div>
-        //     <div className="order-3 mx-auto flex flex-col justify-center gap-7 md:flex-row lg:order-2 lg:mx-0 lg:w-2/4 xl:gap-12">
-        //         <div>
-        //             <p className="mb-2 text-xl font-semibold">Pruduk</p>
-        //             <ul>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Online Course
-        //                     </a>
-        //                 </li>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Software House
-        //                     </a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //         <div>
-        //             <p className="mb-2 text-xl font-semibold">Tim Kami</p>
-        //             <ul>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Ilham Hafidz
-        //                     </a>
-        //                 </li>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Taufan Hidayatul Akbar
-        //                     </a>
-        //                 </li>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Firdan Fauzan
-        //                     </a>
-        //                 </li>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Masnun Muhaemin
-        //                     </a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //         <div>
-        //             <p className="mb-2 text-xl font-semibold">Lainnya</p>
-        //             <ul>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         FAQ
-        //                     </a>
-        //                 </li>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Syarat & Ketentuan
-        //                     </a>
-        //                 </li>
-        //                 <li className="mb-1">
-        //                     <a
-        //                         href=""
-        //                         className="relative z-10 hover:text-[#2276f0] active:font-semibold"
-        //                     >
-        //                         Ketentuan Privasi
-        //                     </a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        //     <div className="order-2 mx-auto flex w-[90%] flex-col gap-2 md:w-[70%] md:flex-row md:items-end md:gap-5 lg:order-3 lg:block lg:w-1/4">
-        //         <div className="mx-auto w-full text-left lg:mx-0">
-        //             <p className="text-center font-semibold md:text-left">
-        //                 Subscribe untuk info terkini!
-        //             </p>
-        //             <input
-        //                 type="email"
-        //                 className="relative z-10 w-full rounded border border-gray-300 bg-[#f4f4f4] py-2"
-        //                 placeholder="Email"
-        //             />
-        //         </div>
-        //         <div>
-        //             <button className="rounded bg-[#2276f0] px-4 py-3 text-sm font-semibold text-white hover:bg-[#2276f0]/80 active:scale-95 md:mt-3">
-        //                 Subcribe
-        //             </button>
-        //         </div>
-        //     </div>
-        //     <div className="order-4 mx-auto block w-full lg:mx-0 lg:hidden">
-        //         {/* <img
-        //             src="/images/contactList.png"
-        //             alt="contact-list"
-        //             className="mt-10"
-        //         /> */}
-        //         <div className="mt-5 flex justify-center gap-4 text-gray-700">
-        //             <a href="https://discord.gg/WV7wAdcp" target="_blank">
-        //                 <IconBrandDiscord className="size-7" />
-        //             </a>
-        //             <a
-        //                 href="https://www.instagram.com/alope.world/"
-        //                 target="_blank"
-        //             >
-        //                 <IconBrandInstagram className="size-7" />
-        //             </a>
-        //             <a
-        //                 href="https://github.com/orgs/Alope-Community/dashboard"
-        //                 target="_blank"
-        //             >
-        //                 <IconBrandGithub className="size-7" />
-        //             </a>
-        //         </div>
-        //         <p className="mt-5">&copy; 2025</p>
-        //     </div>
-        // </footer>
-        <footer className="container mx-auto mt-20 flex flex-wrap gap-10 px-3 py-14 text-center md:px-10 lg:flex-nowrap lg:gap-5 lg:text-left xl:px-5 2xl:px-2">
-            <div className="order-1 w-full text-center">
-                <p className="text-3xl font-bold">ALOPE</p>
-                <p>Tempat belajar koding yang menyenangkan!</p>
-                <div className="text-center">
-                    <div className="mt-5 flex justify-center gap-4 text-gray-700">
-                        <a
-                            href="https://discord.gg/WV7wAdcp"
-                            target="_blank"
-                            aria-label="Join our Discord community"
-                        >
-                            <IconBrandDiscord className="size-7" />
-                        </a>
-                        <a
-                            href="https://www.instagram.com/alope.world/"
-                            target="_blank"
-                            aria-label="Follow our Instagram"
-                        >
-                            <IconBrandInstagram className="size-7" />
-                        </a>
-                        <a
-                            href="https://github.com/orgs/Alope-Community/dashboard"
-                            target="_blank"
-                            aria-label="See our Github community"
-                        >
-                            <IconBrandGithub className="size-7" />
-                        </a>
+        <footer className="bg-primary pb-10 pt-16 text-white">
+            <div className="container mx-auto">
+                <div className="grid grid-cols-12 gap-10 px-4 md:px-10">
+                    <div className="col-span-12 md:col-span-6">
+                        <div className="flex items-center gap-2">
+                            <img
+                                src="/images/Alope.png"
+                                alt="Alope Logo"
+                                className="h-10 w-10 object-contain"
+                            />
+                            <h3 className="text-2xl font-semibold">
+                                Alope Course
+                            </h3>
+                        </div>
+                        <p className="mt-3 text-lg leading-relaxed text-white/90">
+                            Tempat belajar koding yang
+                            <br />
+                            menyenangkan!
+                        </p>
                     </div>
-                    <p className="mt-5">&copy; 2025</p>
+
+                    <div className="col-span-6 md:col-span-2">
+                        <h4 className="mb-3 text-lg font-semibold">Navigasi</h4>
+                        <ul className="space-y-2 text-sm">
+                            <li>
+                                <a href="/courses" className="hover:underline">
+                                    Kursus
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline">
+                                    Artikel
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline">
+                                    Video
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-span-6 md:col-span-2">
+                        <h4 className="mb-3 text-lg font-semibold">Kursus</h4>
+                        <ul className="space-y-2 text-sm">
+                            <li>
+                                <a href="#" className="hover:underline">
+                                    Kursus
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline">
+                                    Artikel
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline">
+                                    Video
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-span-12 md:col-span-2">
+                        <h4 className="mb-3 text-lg font-semibold">Kontak</h4>
+                        <ul className="space-y-3 text-sm">
+                            <li className="flex items-center gap-2">
+                                <IconPilone className="w-4" /> 089123456789
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <IconMap className="w-4" /> Darma, Kuningan,
+                                Jawa Barat
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <IconMail className="w-4" /> contact@alope.id
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="px-4 md:px-10">
+                    <hr className="my-10 border border-white/30" />
+                </div>
+
+                <div className="px-4 md:px-10">
+                    <p className="text-sm text-white/90">
+                        Copyright 2025 by Alope. All Right Reserved
+                    </p>
                 </div>
             </div>
         </footer>
