21f159bb45eb7469478a7fa4e2c87b733082c8bc
MrFatra <ramadhanfatra2004@gmail.com>
2026-01-08 15:34:00 +0700

    remove console log

--- resources/js/Pages/Course/Show.tsx
+++ resources/js/Pages/Course/Show.tsx
@@ -19,7 +19,7 @@ import {
     IconLayersFill,
     IconStarFill,
 } from 'justd-icons';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function highlightGlossary(
     text: string,
@@ -77,11 +77,6 @@ export default function CourseShowPage({
     }[];
     allGlossary: { title: string; description: string; course_id: string }[];
 }) {
-
-    useEffect(() => {
-        console.log(course.modules);
-    }, [])
-
 
     const [activeTab, setActiveTab] = useState<'module' | 'quiz' | 'glosarium'>(
         'module',
@@ -195,8 +190,8 @@ export default function CourseShowPage({
                                 <button
                                     onClick={() => setActiveTab('module')}
                                     className={`rounded-md px-4 py-2 text-sm font-medium transition md:text-base ${activeTab === 'module'
-                                            ? 'bg-[#2276f0] text-white shadow'
-                                            : 'bg-gray-200 text-gray-600'
+                                        ? 'bg-[#2276f0] text-white shadow'
+                                        : 'bg-gray-200 text-gray-600'
                                         }`}
                                 >
                                     Modul
@@ -206,8 +201,8 @@ export default function CourseShowPage({
                                     <button
                                         onClick={() => setActiveTab('quiz')}
                                         className={`rounded-md px-4 py-2 text-sm font-medium transition md:text-base ${activeTab === 'quiz'
-                                                ? 'bg-[#2276f0] text-white shadow'
-                                                : 'bg-gray-200 text-gray-600'
+                                            ? 'bg-[#2276f0] text-white shadow'
+                                            : 'bg-gray-200 text-gray-600'
                                             }`}
                                     >
                                         Quiz
@@ -228,8 +223,8 @@ export default function CourseShowPage({
                                 <button
                                     onClick={() => setActiveTab('glosarium')}
                                     className={`rounded-md px-4 py-2 text-sm font-medium transition md:text-base ${activeTab === 'glosarium'
-                                            ? 'bg-[#2276f0] text-white shadow'
-                                            : 'bg-gray-200 text-gray-600'
+                                        ? 'bg-[#2276f0] text-white shadow'
+                                        : 'bg-gray-200 text-gray-600'
                                         }`}
                                 >
                                     Glosarium
